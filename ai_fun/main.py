64d71da297f4d2f0ba729f959bdf1acfb7edc51f
susu <113452531+susanna3003@users.noreply.github.com>
2024-03-21 17:23:21 +0000

    pushing repl to git

--- ai_fun/main.py
+++ ai_fun/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -11,7 +10,7 @@ def index():
 def user_greet():
     import datetime
     current_datetime = datetime.datetime.now()
-    return f"Hello! Current date and time is {current_datetime}"
+    return f"Hello! Current date and time is {current_datetime}!"
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
