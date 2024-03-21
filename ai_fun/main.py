9a5afa6c08b3ba7a75b7da33e82e0f859062a6f5
susu <113452531+susanna3003@users.noreply.github.com>
2024-03-21 18:50:50 +0000

    I DID THESE!

--- ai_fun/main.py
+++ ai_fun/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,5 +12,28 @@ def user_greet():
     current_datetime = datetime.datetime.now()
     return render_template("user_greet.html", current_datetime=current_datetime)
   
+@app.route('/generate_wanted', methods=['POST'])
+def generate_wanted():
+    # Get form data
+    name = request.form['name']
+    age = request.form['age']
+    dateBorn = request.form['dateBorn']
+    birthPlace = request.form['birthPlace']
+    lastPlace = request.form['lastPlace']
+    favHobby = request.form['favHobby']
+
+    # Generate "WANTED" listing
+    crime_description = f"Suspect's favorite hobby is {favHobby}. Approach with caution."
+    wanted_listing = {
+        'Name': name,
+        'Age': age,
+        'Date of Birth': dateBorn,
+        'Place of Birth': birthPlace,
+        'Last Visited Place': lastPlace,
+        'Crime Description': crime_description
+    }
+
+    return render_template("wanted.html", wanted_listing=wanted_listing)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
